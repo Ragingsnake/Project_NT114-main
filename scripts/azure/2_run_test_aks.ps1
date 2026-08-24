10015f806adefdcf0842a062c26ec888ae773a50
reborn <lamponhq123@gmail.com>
2026-08-24 20:21:57 +0700

    Helm helm helm

--- scripts/azure/2_run_test_aks.ps1
+++ scripts/azure/2_run_test_aks.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = 'Stop'
 $malicious = if ($attackMode -eq "normal") { "false" } else { "true" }
 
 Write-Host "Deploying FL Cluster to AKS via Helm (Attack Mode: $attackMode, Rounds: $rounds)..." -ForegroundColor Cyan
-helm upgrade --install fl-cluster ./helm/nt114-fl --set attackMode=$attackMode --set malicious=$malicious --set rounds=$rounds
+helm upgrade --install fl-cluster ./helm/nt114-fl --namespace nt114-fl --create-namespace --set attackMode=$attackMode --set malicious=$malicious --set rounds=$rounds
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 Write-Host "Deployed! Watch the pods spin up using: kubectl get pods -w" -ForegroundColor Green
